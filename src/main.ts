a4f49c70d18da813c75d1a0cc9bb8516a54fe464
panyanxin <1205307815@qq.com>
2022-03-03 16:53:42 +0800

    feat(global): 引入element-plus ui 库

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+import { globalRegister } from './global'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+// 注册element-plus/其他
+app.use(globalRegister)
+app.use(store).use(router).mount('#app')
